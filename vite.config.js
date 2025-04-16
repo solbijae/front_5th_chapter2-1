a02cb1f851d98f27a4301bc3a7880306774a6a60
Jae <jaeyeonjung3@gmail.com>
2025-04-16 22:07:38 +0900

    [🎉Setup] 심화과제 시작 - 리액트 설치

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vitest/config';
+// import { resolve } from 'path';
+// import { fileURLToPath } from 'url';
+// import { URL } from 'url';
+import react from '@vitejs/plugin-react';
+
+// const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
+  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: 'src/setupTests.js'
+    setupFiles: 'src/setupTests.js',
   },
-})
+  // resolve: {
+  //   alias: {
+  //     '@': resolve(__dirname, './src'),
+  //   },
+  // },
+});
