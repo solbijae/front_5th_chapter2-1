219b19a9599c508864a4648d83c65a94f6ddcbc5
Jae <jaeyeonjung3@gmail.com>
2025-04-16 22:22:33 +0900

    [Chore] vite.config.js 수정

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vitest/config';
-// import { resolve } from 'path';
-// import { fileURLToPath } from 'url';
-// import { URL } from 'url';
+import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { URL } from 'url';
 import react from '@vitejs/plugin-react';
 
-// const __dirname = fileURLToPath(new URL('.', import.meta.url));
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
@@ -13,9 +13,9 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: 'src/setupTests.js',
   },
-  // resolve: {
-  //   alias: {
-  //     '@': resolve(__dirname, './src'),
-  //   },
-  // },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
 });
